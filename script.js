18a84b43b24303a2e14ba415b3fefa2e129fb63a
Munjurina Azam <1521880@iub.edu.bd>
2018-05-23 03:04:34 +0000

    task 5(a)

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
 /* JavaScript code here */
+console.log("external JS");
+//task1
+function sumArray(arr){
+    var sum=0;
+    arr.forEach(function(value,index){
+        
+        sum+=value;
+        
+    });
+    return sum;
+    }
+    console.log(sumArray([1,2,3,4,5]));
+     console.log(sumArray([19,-2,30,-45]));
+    
+    
+
+//task 3
+function checkEmail(emailString){
+    var emailFormat=/^w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var match=emailFormat.test(emailString);
+    return match;
+    
+}
+console.log(checkEmail("ema@ak.com"));
+console.log(checkEmail("ema.info"));
+//task 4
+
+    var library = [
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+function getReadingStatus(index){
+    return library[index].readingStatus;
+    
+}
+console.log(getReadingStatus(2))
+//task 5
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+   //5(a)
+function addItem(newItem){
+    cart.push(newItem);
+    console.log(cart);
+}
+var newItem={name:"Watch",price:64,quantity:1};
+addItem(newItem);
